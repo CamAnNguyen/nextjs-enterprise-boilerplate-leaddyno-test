07120b20dd771e9bc5d73e3af5591c373ae24a2c
An Nguyen <caman.nguyenthanh@gmail.com>
2024-07-18 17:25:14 +0700

    Add LeadDyno tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,14 @@ import "styles/tailwind.css"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <header>
+        <script type="text/javascript" src="https://static.leaddyno.com/js"></script>
+        <script>
+          LeadDyno.key = "1436ca19da13ab3551eb17ecaf4994bba34819e0"
+          LeadDyno.recordVisit();
+          LeadDyno.autoWatch();
+        </script>
+      </header>
       <body>{children}</body>
     </html>
   )
